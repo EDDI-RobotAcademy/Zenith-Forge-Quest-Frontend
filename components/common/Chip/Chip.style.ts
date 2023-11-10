4d89a5289453e14073a883e9eaae9ba507e75435
sbpark <sbpark@ssrinc.co.kr>
2023-11-10 18:49:23 +0900

    update: [박성빈] 커스텀 Chip 공통 컴포넌트 추가 [ZFQF-23]

--- components/common/Chip/Chip.style.ts
+++ components/common/Chip/Chip.style.ts
@@ -5,5 +5,14 @@ import { Chip, css, styled } from '@mui/material'
 export const StyledChip = styled(Chip)(
   ({ theme }) => css`
     color: ${neutralColor.white1};
+    font-weight: 600;
+    &.link-chip {
+      &:hover {
+        opacity: 0.8;
+      }
+    }
+    .MuiChip-label {
+      padding-bottom: 1px;
+    }
   `
 )
