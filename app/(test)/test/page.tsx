36fe72e910fbac57e39741929d9716cabfdd6b3f
sbpark <sbpark@ssrinc.co.kr>
2023-11-10 18:10:34 +0900

    Revert "커스텀 Chip 공통 컴포넌트 추가 [ZFQF-23]"
    This reverts commit f871ecdcfc9883e7602bf6a3df44d642540fd31b.

--- app/(test)/test/page.tsx
+++ app/(test)/test/page.tsx
@@ -1,10 +1,8 @@
-import CommonButton from '@/components/common/Button/Button'
-import Chip from '@/components/common/Chip/Chip'
-import Switch from '@/components/common/Switch/Switch'
+import CommonButton from "@/components/common/Button/Button";
 
 function TestPage() {
   return (
-    <div style={{ paddingTop: '24px' }}>
+    <div>
       <h1>Test Page</h1>
       <CommonButton>나와라</CommonButton>
       <CommonButton>나오긴하네</CommonButton>
@@ -12,20 +10,8 @@ function TestPage() {
       <CommonButton>나와라</CommonButton>
 
       <CommonButton />
-
-      <Chip className="asdasd" size="small" label="test" color="primary" href="test" />
-
-      <Chip size="small" label="Chip Test" color="primary" />
-      <Chip size="medium" label="Chip Test" color="primary" />
-      <Chip size="medium" label="Chip Test" color="secondary" />
-      <Chip size="medium" label="Chip Test" color="error" />
-      <Chip size="medium" label="Chip Test" color="info" />
-      <Chip size="medium" label="Chip Test" color="warning" />
-      <Chip size="medium" label="Chip Test" color="info" />
-
-      <Switch />
     </div>
-  )
+  );
 }
 
-export default TestPage
+export default TestPage;
