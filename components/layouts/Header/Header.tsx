4c9e9d99aa0c02d03ab08d0ee71ec3eba1a79404
sbpark <sbpark@ssrinc.co.kr>
2024-04-09 09:54:41 +0900

    update: [박성빈] Header 컴포넌트 작성 [ZFQF-21]

--- components/layouts/Header/Header.tsx
+++ components/layouts/Header/Header.tsx
@@ -6,14 +6,12 @@ import Link from 'next/link'
 import MenuIcon from '@mui/icons-material/Menu'
 import SquareIconBtn from '@/components/common/SquareIconBtn/SquareIconBtn'
 import SearchIcon from '@mui/icons-material/Search'
-import { useModal } from '@/hooks/useModal'
 import HeaderSearchButton from './HeaderSearchButton'
 import Button from '@/components/common/Button/Button'
 import Tooltip from '@/components/common/Tooltip/Tooltip'
-import GitHubIcon from '@mui/icons-material/GitHub'
-function Header() {
-  const { isOpen, openModal, closeModal } = useModal()
+import Modals from '@/components/common/Modal/Modal'
 
+function Header() {
   return (
     <StyledHeader>
       <Flexbox className="header-container" alignItems="center" justifyContent="space-between">
@@ -23,16 +21,16 @@ function Header() {
           </h1>
 
           <nav className="nav-link-container mobile-hidden">
-            <Link className="navigation left-style" href="">
+            <Link className="navigation left-style" href="/">
               최신 글
             </Link>
-            <Link className="navigation left-style" href="">
+            <Link className="navigation left-style" href="/">
               인기
             </Link>
-            <Link className="navigation left-style" href="">
+            <Link className="navigation left-style" href="/">
               Q & A
             </Link>
-            <Link className="navigation left-style" href="">
+            <Link className="navigation left-style" href="/">
               채용 공고
             </Link>
           </nav>
@@ -40,7 +38,7 @@ function Header() {
 
         <div className="header-right">
           <div className="mobile-only">
-            <SquareIconBtn onClick={openModal} className="icon search" color="primary" aria-label="search">
+            <SquareIconBtn className="icon search" color="primary" aria-label="search">
               <SearchIcon color="primary" />
             </SquareIconBtn>
 
@@ -53,9 +51,11 @@ function Header() {
 
           <div className="user-btn-container mobile-hidden">
             <HeaderSearchButton />
-            <Button startIcon={<GitHubIcon color="actions" />} className="login-btn" size="small" variant="contained">
-              GitHub 로그인
+            <Button className="login-btn" type="button" size="small" variant="contained">
+              로그인
             </Button>
+
+            <Modals />
           </div>
         </div>
       </Flexbox>
